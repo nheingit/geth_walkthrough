7afbd8ed57d99ea16c139dd3cbf48d9e73f0fdb2
Noah <nheindev@gmail.com>
2021-04-05 19:33:50 -0500

    added the account balance file

---
+++ account_balance.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"math"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+  "github.com/ethereum/go-ethereum/ethclient"
+)
+
+func main() {
+	client, err := ethclient.Dial("https://mainnet.infura.io")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// this will give you the latest balance of the account number
+	account := common.HexToAddress("0x71c7656ec7ab88b098defb751b7401b5f6d8976f")
+	balance, err := client.BalanceAt(context.Background(), account, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(balance)
+  // this will give you the balance at the time of blockNumber
+	blockNumber:= big.NewInt(5532993)
+	balanceAt, err := client.BalanceAt(context.Background(), account, blockNumber)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(balanceAt)
+
+	// ETH uses fixed-point precision, which is wei
+	// This means we need to convert the large numbers into the actual eth value
+	// calculation is == (wei/10^18)
+	fbalance := new(big.Float)
+	fbalance.SetString(balance.String())
+	ethValue := new(big.Float).Quo(fbalance, big.NewFloat(math.Pow10(18)))
+
+	fmt.Println(ethValue)
+
+	//If you want to get the pending balance instead of the current balance, for example when you are waiting for a transaction to be confirmed
+	// instead of using the BalanceAt() function, we can use PendingBalanceAt()
+
+	pendingBalance, err := client.PendingBalanceAt(context.Background(), account)
+	if err != nil{
+		log.Fatal(err)
+	}
+	fmt.Println(pendingBalance)
+
+
+
+
+}
---
+++ generate_wallet.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"crypto/ecdsa"
+    "fmt"
+    "log"
+
+    "github.com/ethereum/go-ethereum/common/hexutil"
+    "github.com/ethereum/go-ethereum/crypto"
+    "github.com/ethereum/go-ethereum/crypto/sha3"
+)
